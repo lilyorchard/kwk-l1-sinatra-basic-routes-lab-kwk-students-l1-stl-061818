6c7daab468c705018bcaa1577e12c037176b8dc7
lilyorchard <lily@orchardfamily.com>
2018-06-26 16:28:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
-require_relative 'config/environment'
+require 'sinatra'
 
 class App < Sinatra::Base
+  
+  get '/' do
+  end
+  
+  get '/name' do
+    "My name is Lily"
+  
+end
+  
+  get  '/hometown' do
+    "My hometown is St. Louis"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is 'The Way You Look Tonight'"
+  end
 end
